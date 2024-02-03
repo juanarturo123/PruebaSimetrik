b0f864f18f69c295793d6e3c8e0cdf8711b2e836
Juan Arturo Jurado <juan.arturo@sofka.local>
2024-02-02 19:11:46 -0500

    Automatización

---
+++ testSimetrik/src/test/java/pages/PaginaPrincipal.java
@@ -0,0 +1,15 @@
+package pages;
+ 
+public class PaginaPrincipal extends BasePage {
+ 
+    public PaginaPrincipal() {
+        super(driver);
+    }
+ 
+    // Método para navegar a www.freerangetesters.com
+    public void navigateToFreeRangeTesters() {
+        navigateTo("https://www.google.com");
+    }
+ 
+}
+ 
---
+++ testSimetrik/src/test/resources/FreeRangeNavegacion.feature
@@ -0,0 +1,4 @@
+Feature: Navigation Google
+ 
+Scenario: I can access the subpages through the navigation bar
+    Given I navigate to www.google.com
